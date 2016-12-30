7f667d4d0a2fe85d69dcc92472e01c68c6ea2cf7
Luis Antunes <luisantunes.developer@gmail.com>
2016-12-30 19:43:34 +0000

    refactor core to a more functional and consistent programming style

--- cli/cli.js
+++ cli/cli.js
@@ -1,35 +1,18 @@
 'use strict';
 
-const fs = require('fs');
-const dt = require('../dt');
+const dt = require('../core').dt;
 const chalk = require('chalk');
 
-// process command line arguments
-if (process.argv.length < 5) {
-	console.log(chalk.red("SYNOPSIS"));
-    console.log(chalk.red(  __filename + " [link numberOfRequests outputFileExtension [outputDir]]"));
-    process.exit();
-}
+const args = {
+	location: 'http://www.google.com',
+	numberOfRequests: 10,
+	outputFileExtension: 'html'
+};
 
-// arguments & defaults
-const link = process.argv[2];
-const numberOfRequests = parseInt(process.argv[3]);
-const outputFileExtension = process.argv[4];
-const outputDir = process.argv[5];
-const defaultOutputDir = './downloads';
-const downloadOutputDirectory = outputDir ? `${__dirname}/${outputDir}` : `./downloads`;
+const listener = dt.execute(args);
+const time = process.hrtime()
 let nrRequestsFinished = 0;
 
-// create dir if does not exist
-if (!fs.existsSync(downloadOutputDirectory)){
-    fs.mkdirSync(downloadOutputDirectory);
-}
-
-// make the requests
-console.log(chalk.green(`${numberOfRequests} requests for ${link}...`));
-console.log(chalk.green(`Output directory: ${downloadOutputDirectory}`));
-const listener = dt(link, numberOfRequests, outputFileExtension, downloadOutputDirectory);
-const time = process.hrtime();
 
 //listen the events
 listener.on('progress', (request) => {
@@ -41,12 +24,11 @@ listener.on('error', (request) => {
 });
 
 listener.on('end', (request) => {
-	nrRequestsFinished++;
-	if (++nrRequestsFinished >= numberOfRequests) {
+	++nrRequestsFinished;
+	if (nrRequestsFinished >= args.numberOfRequests) {
 		const diff = process.hrtime(time);
 		console.log(chalk.green(`${nrRequestsFinished} requests finished with sucess in ${diff} seconds`));
 		process.exit();
 	}
 	console.log(chalk.green(`Request ${request.number} has finished with sucess`));
 });
-
